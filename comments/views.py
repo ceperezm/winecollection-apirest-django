d4c693c5b12a2d2bb71a97f35279f14c14f188f4
CamiloPerezMoreno <camilohex@outlook.com>
2026-09-24 14:31:43 -0500

    feat(comments): add search filters, routing IDs, and validation rules
    - Add search and ordering filters to comment viewsets
    - Restrict search fields strictly to collection_name and wine_name
    - Expose collection_id and wine_id in read serializers for frontend routing
    - Implement validation rule requiring a minimum of 5 wines to comment on a collection

--- comments/views.py
+++ comments/views.py
@@ -5,6 +5,7 @@ from .permissions import CanViewComment
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, serializers
 import django_filters.rest_framework
+from rest_framework import filters
 from wine_collection_api.pagination import CommentPagination
 from drf_spectacular.utils import extend_schema
 
@@ -19,8 +20,10 @@ ProviderCollectionReadCommentSerializer, ProviderCollectionWriteCommentSerialize
 @extend_schema(tags=['Comments - Wines'])
 class WineCommentViewSet(viewsets.ModelViewSet):
     pagination_class = CommentPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['wine']
+    search_fields = ['wine__name']
+    ordering_fields = ['comment_date']
 
     def get_queryset(self):
         user = self.request.user
@@ -72,8 +75,10 @@ class ClientCollectionCommentViewSet(viewsets.ModelViewSet):
     queryset = ClientCollectionComment.objects.all().order_by('-comment_date')
     permission_classes = [IsAuthenticated]
     pagination_class = CommentPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['collection']
+    search_fields = ['collection__collection_name']
+    ordering_fields = ['comment_date']
 
     def get_permissions(self):
         """
@@ -100,8 +105,10 @@ class ClientCollectionCommentViewSet(viewsets.ModelViewSet):
 class ProviderCollectionCommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = CommentPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['collection']
+    search_fields = ['collection__collection_name']
+    ordering_fields = ['comment_date']
 
     def get_queryset(self):
         user = self.request.user
@@ -144,3 +151,4 @@ class ProviderCollectionCommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Serializer now handles User to Client conversion
         serializer.save()
+
